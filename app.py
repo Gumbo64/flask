33f71ac1de6ba9c1c4056b84e6e36215701cdb95
Gumbo64 <chickennuggetcrazykid@gmail.com>
2019-11-25 19:22:06 +1100

    sqlalchemy

--- app.py
+++ app.py
@@ -5,24 +5,9 @@ from flask import Markup
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user
 from datetime import datetime
-
 import jinja2
 
-import shelve
-global indivtext
-indivtext = []
 
-
-try:
-    savefile = shelve.open('savefile')
-    indivtext = savefile['indivtext']
-except:
-    pass
-
-
-
-global username
-username = "Anon"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
@@ -52,41 +37,65 @@ class commentform(FlaskForm):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route('/')
 def home():
     global number
     number=1
-    return render_template("home.html", username=username)
+    return render_template("home.html", name= 'a')
 
 @app.route('/yoda')
 def yoda():
     return render_template("yoda.html")
 
-@app.route('/forms', methods=['GET', 'POST'])
-def form():
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
     form = LoginForm()
-    global username
     if form.validate_on_submit():
-        username = form.username.data
-        if username == 'kevin':
-            return render_template('kevinpage.html')
-        else: 
-            return redirect("/", code=302)
+        user = User(name = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/login", code=302)
+    return render_template('signup.html', form=form)
 
-    return render_template('form.html', form=form)
-
+@app.route('/login', methods=['GET', 'POST'])
+def loginform():
+    loginform = LoginForm()
+    if loginform.validate_on_submit():
+        pass
+    return render_template('login.html',form=loginform)
 @app.route('/chatroom', methods=['GET', 'POST'])
-def form1():
-    form1 = commentform()
-    global username
-    global indivtext
-    if form1.validate_on_submit():
-        comment = form1.comment.data
-        addable= str(username) +": "+ str(comment)
-        indivtext.append(addable)
-        savefile = shelve.open('savefile')
-        savefile['indivtext']=indivtext
-    return render_template('chatroom.html',indivtext=indivtext, form=form1, username=username)
+def chatroom():
+    return 'e'
+    
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
+
+
+
+
+
+# #old code
+# #  try:
+# #     pass
+# # except:
+# #     form1 = commentform()
+# #     global username
+# #     global indivtext
+# #     if form1.validate_on_submit():
+# #         comment = form1.comment.data
+# #         addable= str(username) +": "+ str(comment)
+# #         indivtext.append(addable)
+# #         savefile = shelve.open('savefile')
+# #         savefile['indivtext']=indivtext
+# #     return render_template('chatroom.html',indivtext=indivtext, form=form1, username=username)
+
+
+
+# #     if username == 'kevin':
+# #             return render_template('kevinpage.html')
+# #     else: 
+# #             return redirect("/", code=302)
+ 
